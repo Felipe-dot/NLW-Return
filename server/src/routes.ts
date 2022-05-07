bf0961a20b59d049c57da6494e6181e1078559b7
Luis Felipe de Azevedo Melo <felipeluis610@gmail.com>
2022-05-06 21:23:21 -0300

    Adding automated tests to the application
    Creating some unit test with jest  and making some
    contract rules

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,7 +1,5 @@
 import express from 'express';
-import nodemailer from 'nodemailer';
 import { NodemailerMailAdapter } from './adapters/nodemailer/nodemailer-mail-adapter';
-import {prisma} from './prisma';
 import { PrismaFeedbacksRepository } from './repositories/prisma/prisma-feedbacks-repository';
 import { SubmitFeedbackUseCase } from './use-cases/submit-feedback-use-case';
 
@@ -12,6 +10,7 @@ routes.post('/feedbacks', async (req,res) => {
 
     const prismaFeedbackRepository = new PrismaFeedbacksRepository();
     const nodemailerMailAdapter = new NodemailerMailAdapter();
+   
     const submitFeedbackUseCase = new SubmitFeedbackUseCase(
         prismaFeedbackRepository,
         nodemailerMailAdapter,
